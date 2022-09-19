03759acbc8f29f83839b8cb2988341d1bc4757e9
matthew2morris <matthew2morris@gmail.com>
2022-09-19 09:49:42 -0400

    added restart calculator feature with validation

--- JS101/lesson_2/calculator.js
+++ JS101/lesson_2/calculator.js
@@ -3,6 +3,7 @@
 // Ask user for operator type
 // Perform operation
 // Print operation result to terminal 
+// Ask user if they want to do another calculation
 
 const READLINE = require("readline-sync");
 
@@ -14,52 +15,65 @@ function invalidNumber(number) {
   return number.trim() === '' || Number.isNaN(Number(number));
 }
 
+let restartCalc;
+
 prompt("Welcome to Calculator!");
 
-prompt("What's your first number?");
-let num1 = READLINE.question();
+do {
+  prompt("What's your first number?");
+  let num1 = READLINE.question();
 
-while (invalidNumber(num1)) {
-  prompt("Hmm... that doesn't look like a valid number. Please try again:");
-  num1 = READLINE.question();
-}
+  while (invalidNumber(num1)) {
+    prompt("Hmm... that doesn't look like a valid number. Please try again:");
+    num1 = READLINE.question();
+  }
 
-prompt("What's your second number?");
-let num2 = READLINE.question();
+  prompt("What's your second number?");
+  let num2 = READLINE.question();
 
-while (invalidNumber(num2)) {
-  prompt("Hmm... that doesn't look like a valid number. Please try again:");
-  num2 = READLINE.question();
-}
+  while (invalidNumber(num2)) {
+    prompt("Hmm... that doesn't look like a valid number. Please try again:");
+    num2 = READLINE.question();
+  }
 
-prompt("What kind of operation do you want to do? (add|subtract|multiply|divide)");
-let operator = READLINE.question();
+  prompt("What kind of operation do you want to do? (add|subtract|multiply|divide)");
+  let operator = READLINE.question();
 
-while (!['add', 'subtract', 'multiply', 'divide'].includes(operator)) {
-  prompt('Must choose: add, subtract, multiply or divide');
-  operator = READLINE.question();
-}
-// Add development branch
-let test = "abc";
-// Perform operation
-let output;
-switch (operator) {
-  case 'add':
-    output = (Number(num1) + Number(num2));
-    break;
-  case 'subtract':
-    output = (Number(num1) - Number(num2));
-    break;
-  case 'multiply':
-    output = (Number(num1) * Number(num2));
-    break;
-  case 'divide':
-    output = (Number(num1) / Number(num2));
-    break;
-  default:
-    output = "error: invalid operator type";
-    break;
-}
+  while (!['add', 'subtract', 'multiply', 'divide'].includes(operator)) {
+    prompt('Must choose: add, subtract, multiply or divide');
+    operator = READLINE.question();
+  }
 
+  // Perform operation
+  let output;
+  switch (operator) {
+    case 'add':
+      output = (Number(num1) + Number(num2));
+      break;
+    case 'subtract':
+      output = (Number(num1) - Number(num2));
+      break;
+    case 'multiply':
+      output = (Number(num1) * Number(num2));
+      break;
+    case 'divide':
+      output = (Number(num1) / Number(num2));
+      break;
+    default:
+      output = "error: invalid operator type";
+      break;
+  }
 
-prompt(`The result equals ${output}`);
+  prompt(`The result equals ${output}`);
+
+  prompt(`Do you want to do another calculation? (y/n) `)
+  restartCalc = READLINE.question();
+
+  while (!['y', 'n'].includes(restartCalc)) {
+    prompt(`Hmmmm... please answer with y/n: `)
+    restartCalc = READLINE.question();
+  }
+
+} while (restartCalc === 'y')
+
+prompt(`Thanks for using Calculator!`)
