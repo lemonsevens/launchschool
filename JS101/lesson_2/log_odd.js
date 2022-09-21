e17f5f8851de7433103e6861b063b68ad0aa7389
matthew2morris <matthew2morris@gmail.com>
2022-09-21 15:56:30 -0400

    adding log_odd.js

---
+++ JS101/lesson_2/log_odd.js
@@ -0,0 +1,7 @@
+// Log all odd numbers from 1 to 99, inclusive, to the console, with each number on a separate line.
+// INPUT: nothing
+// OUTPUT: console.log odd numbers only (1-99 inclusive) with each number on a separate line
+
+for (let i = 1; i <= 99; i += 2) {
+    console.log(i);
+}
