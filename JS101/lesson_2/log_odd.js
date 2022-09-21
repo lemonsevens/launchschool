720fda810976df6afc5bfd55335d5088e09a36cc
matthew2morris <matthew2morris@gmail.com>
2022-09-21 16:01:17 -0400

    added user input capability

--- JS101/lesson_2/log_odd.js
+++ JS101/lesson_2/log_odd.js
@@ -2,6 +2,11 @@
 // INPUT: nothing
 // OUTPUT: console.log odd numbers only (1-99 inclusive) with each number on a separate line
 
-for (let i = 1; i <= 99; i += 2) {
+const READLINE = require('readline-sync');
+
+console.log("What number should we limit the logging to? ");
+let answer = READLINE.question();
+
+for (let i = 1; i <= answer; i += 2) {
     console.log(i);
 }
