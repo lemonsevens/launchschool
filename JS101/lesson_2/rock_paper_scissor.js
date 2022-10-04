3dfffb239ee758cdd743f79e457d20d24694959b
matthew2morris <matthew2morris@gmail.com>
2022-10-04 13:43:06 -0400

    adding all modified files

--- JS101/lesson_2/rock_paper_scissor.js
+++ JS101/lesson_2/rock_paper_scissor.js
@@ -1,30 +1,37 @@
-const readline = require("readline-sync");
-const MESSAGES = require("./rock_paper_scissor_messages.json")
+const readline = require('readline-sync');
+const MESSAGES = require('./rock_paper_scissor_messages.json');
+
 const CHOICES_AND_WINNERS = {
   rock: { validChoices: ['r', 'rock'], defeats: ['scissors'] },
   paper: { validChoices: ['p', 'paper'], defeats: ['rock'] },
-  scissors: { validChoices: ['s', 'scissors'], defeats: ['paper'] }
-}
+  scissors: { validChoices: ['s', 'scissors'], defeats: ['paper'] },
+};
 const CHOICES = Object.keys(CHOICES_AND_WINNERS);
 const WINNING_SCORE = 3;
 let userScore = 0;
 let cpuScore = 0;
 
 // Helper Functions
-function prompt(key) {
-  let message = messages(key)
-  console.log(`=> ${message}`);
-}
 
 function messages(value) {
   return MESSAGES[value];
 }
 
+function prompt(key, str) {
+  const message = messages(key);
+  if (str) {
+    console.log(`=> ${message} ${str}`);
+  } else {
+    console.log(`=> ${message}`);
+  }
+}
+
 function validate(str) {
   let validated;
+  // eslint-disable-next-line no-restricted-syntax
   for (const key in CHOICES_AND_WINNERS) {
     if (validated === undefined || validated === false) {
-        validated = (CHOICES_AND_WINNERS[key]['validChoices'].includes(str));
+      validated = (CHOICES_AND_WINNERS[key].validChoices.includes(str));
     }
   }
   return validated;
@@ -32,10 +39,10 @@ function validate(str) {
 
 // Core Functions
 function userChoice() {
-  let userChoice = readline.question();
+  let userChoice = readline.question().toLowerCase();
   while (validate(userChoice) === false) {
-    prompt('validChoiceError') + CHOICES.join(', ');
-    userChoice = readline.question();  
+    prompt('validChoiceError', CHOICES.join(', '));
+    userChoice = readline.question().toLowerCase();
   }
   if (userChoice === 'r') {
     userChoice = 'rock';
@@ -48,20 +55,20 @@ function userChoice() {
 }
 
 function cpuChoice() {
-  let randomIndex = Math.floor(Math.random() * CHOICES.length);
-  let cpuChoice = CHOICES[randomIndex];
+  const randomIndex = Math.floor(Math.random() * CHOICES.length);
+  const cpuChoice = CHOICES[randomIndex];
   return cpuChoice;
 }
 
 function winnerCalc(user, cpu) {
   let winner;
-  if (CHOICES_AND_WINNERS[user]['defeats'].includes(cpu)) {
-    winner = "User";
+  if (CHOICES_AND_WINNERS[user].defeats.includes(cpu)) {
+    winner = 'User';
     userScore += 1;
   } else if (user === cpu) {
-    winner = "Tie";
+    winner = 'Tie';
   } else {
-    winner = "CPU";
+    winner = 'CPU';
     cpuScore += 1;
   }
   return winner;
@@ -69,25 +76,25 @@ function winnerCalc(user, cpu) {
 
 function weGotAWinner() {
   if (userScore === WINNING_SCORE) {
-    return ("User won the match!");
-  } else if (cpuScore === WINNING_SCORE) {
-    return ("CPU won the match!");
-  } else {
-    return false;
+    return ('User won the match!');
   }
+  if (cpuScore === WINNING_SCORE) {
+    return ('CPU won the match!');
+  }
+  return false;
 }
 
 function playAgain() {
   prompt('playAgain');
-  let playAgain = readline.question();
+  let playAgain = readline.question().toLowerCase();
   while (playAgain[0] !== 'n' && playAgain[0] !== 'y') {
     prompt('playAgainError');
-    playAgain = readline.question();
+    playAgain = readline.question().toLowerCase();
   }
   return playAgain;
 }
 
-// Core Program 
+// Core Program
 let playAgainResult;
 
 do {
@@ -95,27 +102,25 @@ do {
   do {
     prompt('makeChoice');
 
-    let userResult = userChoice();
-    let cpuResult = cpuChoice();
-    let winnerCalcResult = winnerCalc(userResult, cpuResult);
+    const userResult = userChoice();
+    const cpuResult = cpuChoice();
+    const winnerCalcResult = winnerCalc(userResult, cpuResult);
 
-    console.log(" ");
+    console.log(' ');
     console.log(`User: ${userResult}`);
     console.log(`CPU: ${cpuResult}`);
     console.log(`Won Round: ${winnerCalcResult}`);
     console.log(`Match Score: ${userScore} - ${cpuScore}`);
-    console.log(" ");
+    console.log(' ');
+  } while (weGotAWinner() === false);
 
-  } while (weGotAWinner() == false)
-
-  console.log("-----------------------------");
+  console.log('-----------------------------');
   console.log(weGotAWinner());
-  console.log("-----------------------------");
+  console.log('-----------------------------');
 
   playAgainResult = playAgain();
 
   console.clear();
   userScore = 0;
   cpuScore = 0;
-
-} while (playAgainResult[0] !== 'n')
+} while (playAgainResult[0] !== 'n');
