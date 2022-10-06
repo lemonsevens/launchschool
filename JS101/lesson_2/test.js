e0143c54fa26a25001ae85fe408abee72bd3f942
matthew2morris <matthew2morris@gmail.com>
2022-10-06 09:59:07 -0400

    added lesson_3 directory

--- JS101/lesson_2/test.js
+++ JS101/lesson_2/test.js
@@ -1,1 +1,12 @@
-console.log('hello')
+function foo(number) {
+  return number;
+}
+
+let number = 1;
+let newNumber = foo(number);
+console.log(number);    // 1
+console.log(newNumber); // 1
+
+number = 3;
+console.log(number);    // 3
+console.log(newNumber); // 1
