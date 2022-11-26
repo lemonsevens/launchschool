c328e9b2c12995dc3f1921044a733a1259fb6b41
matthew2morris <matthew2morris@gmail.com>
2022-11-26 08:59:42 -0500

    twentyone.js init

---
+++ JS101/lesson_6/twentyone.js
@@ -0,0 +1,25 @@
+/*
+DESCRIPTION:
+  Create a game of twenty one. 
+
+RULES:
+  - Deck: 
+    - 52 cards
+      - 4 suits (hearts, diamonds, spades, clubs)
+      - 13 values (2 - 10, jack, queen, king, ace)
+  - Goal: 
+    - Player closest to 21 wins
+    - if you go over: you bust (lose)
+  - Setup: 
+    - Two players:
+      - dealer
+      - player
+    - Initial draw of two cards each
+    - Players can see only one (top) card of other player
+  - Card Values:
+    - 2-10 = card value
+    - jack, queen and king = 10
+    - ace = 1 or 11
+
+
+*/
