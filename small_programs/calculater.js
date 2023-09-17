aef7f22aec3a6f798d1d682b5f6c3e520065ff49
matthew2morris <matthew2morris@gmail.com>
2023-09-17 09:59:38 -0400

    added calculater.js

---
+++ small_programs/calculater.js
@@ -0,0 +1,21 @@
+// Ask the user for the first number.
+// Ask the user for the second number.
+// Ask the user for an operation to perform.
+// Perform the operation on the two numbers.
+// Print the result to the terminal.
+
+console.log("Welcome to the Command Line Calculater!");
+
+const readline = require('readline-sync');
+
+let userInputFirstNum = readline.question("Enter first number: ");
+let userInputSecondNum = readline.question("Enter second number: ");
+let userInputOperationType = readline.question("Enter operation type: ");
+
+
+
+function calculaterProcess(firstNum, secondNum, operationType) {
+
+}
+
+console.log(`Your calculation result is: ${calculaterProcess(userInputFirstNum, userInputSecondNum, userInputOperationType)}`);
